0e88ae65d2c6a8eeaf193898845fd8c929abfa0f
minechoo <choominhae@gmail.com>
2023-08-18 13:09:47 +0900

    메뉴 활성화

--- components/atoms/text/Text.jsx
+++ components/atoms/text/Text.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { Nanum_Myeongjo } from 'next/font/google';
 import { Work_Sans } from 'next/font/google';
 import { Orbitron } from 'next/font/google';
+import { useRouter } from 'next/router';
 
 const nanum = Nanum_Myeongjo({
 	subsets: ['latin'],
@@ -29,11 +30,21 @@ const orbitron = Orbitron({
 
 //React.createElement('elementType:striing', props:object, children:React Mode)
 function Text({ children, url, style, className, type, tag = 'p', isOn = false }) {
+	const router = useRouter();
+	const currentPath = router.pathname;
 	return React.createElement(
 		tag, //elementType
 		{
 			//props
-			className: clsx(styles.txt, className, nanum.variable, work.variable, styles[`txt_${type}`], isOn && styles.on),
+			className: clsx(
+				currentPath === url ? styles.on : '',
+				styles.txt,
+				className,
+				nanum.variable,
+				work.variable,
+				styles[`txt_${type}`],
+				isOn && styles.on
+			),
 			//전달되는 boolean값에 따라 고유클래스 on추가, module.sass가 자체적으로 고유클래스명으로 변환하기 때문에 부모의 클래스명을 내부 전용 css에 연결하는게 불가
 			style: url ? style : { ...style, transitionDuration: '0.5s' },
 			onMouseEnter: (e) => (e.target.style.color = style?.hoverColor),
