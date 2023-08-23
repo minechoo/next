1746186473c08a2d93c110fa8438e77dcae78a32
minechoo <choominhae@gmail.com>
2023-08-23 11:46:53 +0900

    페이지 전환효과

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,40 @@ import '@/styles/globals.scss';
 import axios from 'axios';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
 
 axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<AnimatePresence mode='wait'>
+				<motion.div key={router.pathname}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+					{/* 페이지가 바뀌기 시작할때 나타날 프레임 */}
+					<motion.div
+						className='in'
+						initial={{ scaleX: 0 }}
+						animate={{ scaleX: 0 }}
+						exit={{ scaleX: 1 }}
+						transition={{ duration: 1, ease: [0.17, 0.67, 0.83, 0.67] }}
+					></motion.div>
+					{/* 페이지가 바뀌고 나서 사라질 프레임 */}
+					<motion.div
+						className='out'
+						initial={{ scaleX: 1 }}
+						animate={{ scaleX: 0 }}
+						exit={{ scaleX: 0 }}
+						transition={{ duration: 1, ease: [0.17, 0.67, 0.83, 0.67] }}
+					></motion.div>
+				</motion.div>
+			</AnimatePresence>
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	);
